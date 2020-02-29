476eaf43243ae0c7c85a33b77b61779208a54c36
luckyjiachuixun <luckyjiacx@sina.com>
2020-02-29 18:44:56 +0800

    把本地工程添加到git

---
+++ GitTest/src/com/jia/git/Demo1.java
@@ -0,0 +1,12 @@
+package com.jia.git;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
